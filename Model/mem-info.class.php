3457593d9521d8c1a9b733dcdb68346512f80b67
givemesleep <bellenjustice01@gmail.com>
2025-06-10 17:25:42 +0800

    New OOP

--- Model/mem-info.class.php
+++ Model/mem-info.class.php
@@ -1,11 +1,31 @@
-<?php 
+<?php
 
-class MemberInfo extends DB_Connection {
-    
+class MemberInfo extends DB_Connection
+{
+    private $ADDMEM;
+    private $UPDATE;
+    private $SOFTDEL;
+    private $VIEWS;
+
     //Add Public Function
+    public function AddMember()
+    {
+        $ADDNEW = $this->ADDMEM = "INSERT INTO () VALUES ()";
+        $DATA = [];
+        $STMTADD = $this->conn->prepare($ADDNEW);
+        $STMTADD->execute($DATA);
+
+        header("location : index.php?Success=Success");
+        exit();
+    }
+    //Update Function
+    public function UpdateMember($UID) {}
+    //Soft-Delete
+    public function SoftDel($UID) {}
+    //View Files
+    public function ViewMem() {}
 
     //Database Security
 }
-
 
 ?>
--- process/addmem.proc.php
+++ process/addmem.proc.php
@@ -1,21 +1,18 @@
-<?php 
+<?php
 
 //Isset or the submitted forms first (POST Method)
 
-if(isset($_POST['submit'])){
-
+if (isset($_POST["submit"])) {
     //Post Method Form inputs
-    
+
     //Require the Model, Controller and the Database
-    require_once '../Model/config.db.php'; //Database Connection
-    require_once '../Model/mem-info.class.php'; //Model
-    require_once '../Controller/newmem.ctrl.php'; //Controller
+    require_once "../Model/config.db.php"; //Database Connection
+    require_once "../Model/mem-info.class.php"; //Model
+    require_once "../Controller/newmem.ctrl.php"; //Controller
 
     //Add the Controller and Initiate an instance to the public function that will run onto model
     $Member = new MemberInfoCtrl();
     //Add the public function
 }
-
-
 
 ?>
